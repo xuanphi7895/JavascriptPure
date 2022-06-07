c15dd33d380052dd609559fe678501da4d30e364
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2022-06-07 07:28:54 +0700

    add page sign up

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,20 @@
 // https://smartlearning.vn/account/login/submit
 
 $(document).ready(function() {
-    $('#tab-login').on('click', function() {
+    $('#login').on('click', function() {
+        $(".nav-pills .nav-link").removeClass("active");
+        $(this).addClass("active");
         $('#form-login').css("display", "block");
         $('#form-signup').css("display", "none");
     });
-    $('#tab-signup').on('click', function() {
+    $('#signup').on('click', function() {
+        $(".nav-pills .nav-link").removeClass("active");
+        $(this).addClass("active");
         $('#form-signup').css("display", "block");
-        $('#form-login').css("display", "none");
+        $('#form-login').css({ "display": "none" });
     });
 
-    $("form").submit(function(event) {
+    $("#form-login").submit(function(event) {
         var formData = {
             username: $("#username").val(),
             password: $("#password").val(),
@@ -25,9 +29,40 @@ $(document).ready(function() {
             contentType: "application/json; charset=utf-8",
         }).done(function(data) {
             if (data.statusCode === 200) {
-                alert("Login thành công!");
+                alert("Login sucess!");
             } else {
-                alert("Login không thành công!");
+                alert("Login not success!");
+            }
+        });
+
+        event.preventDefault();
+    });
+    $("#form-signup").submit(function(event) {
+        var formData = {
+            username: $("#username1").val(),
+            password: $("#password1").val(),
+            email: $("#email").val(),
+            firstName: "TRAN",
+            lastName: "Phi",
+            confirmPassword: $("#password1").val(),
+            mobile: "12345678",
+            address: "Hue",
+            birthDate: "2022-06-07",
+            gender: 1
+        };
+        console.log(formData);
+        $.ajax({
+            type: "POST",
+            url: "https://smartlearning.vn/register/submit",
+            data: JSON.stringify(formData),
+            dataType: "json",
+            encode: true,
+            contentType: "application/json; charset=utf-8",
+        }).done(function(data) {
+            if (data.statusCode === 200) {
+                alert("Sign up success!");
+            } else {
+                alert("Sign up not success!");
             }
         });
 
